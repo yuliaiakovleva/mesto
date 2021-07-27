dbc8e5f3290f773fe26655104a72e0aed03eedf5
Yulia Iakovleva <mme.yuliaiakovleva@yandex.ru>
2021-07-27 17:51:37 +0300

    feat: добавить взаимодействие с попапом чреез js-код

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+//let content = document.querySelector('.content');
+let editButton = document.querySelector('.form__edit-btn');
+let popup = document.querySelector('.popup');
+let closeButton = document.querySelector('.popup__close-btn');
+let popupName = document.querySelector('.popup__text_name');
+let popupInfo = document.querySelector('.popup__text_info');
+let inputName = document.querySelector('.input__text_type_name');
+let inputInfo = document.querySelector('.input__text_type_info');
+let form = document.querySelector('.form');
+
+let formElement = document.querySelector('.popup__container');
+
+function popupOpened () {
+    popup.classList.add('popup_opened');
+    popupName.value = inputName.textContent;
+    popupInfo.value = inputInfo.textContent;
+}
+
+function popupClosed () {
+    popup.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', popupOpened);
+closeButton.addEventListener('click', popupClosed);
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+    inputName.textContent = popupName.value;
+    inputInfo.textContent = popupInfo.value;
+    popupClosed ();
+}
+
+formElement.addEventListener('submit', formSubmitHandler); 
+
+
+
